899a76f506985fea7c5bef672c5c5e789ca8741e
Jeongjiw00 <wldn0229@naver.com>
2022-12-29 12:15:19 +0900

    second commit

--- validation.js
+++ validation.js
@@ -2,13 +2,22 @@ module.exports = {
   isEmail: (value) => {
     //value값이 있으면 email에 value, 없으면 빈문자열
     const email = value || "";
+    //@가 여러개 들어가면 오류가 생기므로 etc인자도 만들어줌
+    const [localPart, domain, ...etc] = email.split("@");
 
     //@하나만 + 공백미포함 + 이메일젤앞에-안됨
-    if (email.split("@").length !== 2) {
+    if (!localPart || !domain || etc.length !== 0) {
       return false;
     } else if (email.includes(" ")) {
       return false;
     } else if (email[0] === "-") {
+      return false;
+    }
+
+    //localPart, domain 조건
+    const localPartCheck = /^[a-zA-Z0-9_+-]+$/;
+    const domainCheck = /^[a-zA-Z0-9.-]+$/;
+    if (!localPartCheck.test(localPart) || !domainCheck.test(domain)) {
       return false;
     }
 
--- validation.spec.js
+++ validation.spec.js
@@ -6,7 +6,6 @@ test('입력한 이메일 주소에는 "@" 문자가 1개만 있어야 이메일
   expect(isEmail("sparta00@naver.com")).toEqual(true);
   expect(isEmail("sparta00@@@@naver.com")).toEqual(false);
   expect(isEmail("sparta00naver.com")).toEqual(false);
-  expect(isEmail("sparta00naver.com@")).toEqual(true);
 });
 
 test("입력한 이메일 주소에 공백(스페이스)이 존재하면 이메일 형식이 아니다.", () => {
@@ -21,4 +20,20 @@ test("입력한 이메일 주소 맨 앞에 하이픈(-)이 있으면 이메일 
   expect(isEmail("sparta-00-@-na--ver-.c-o-m")).toEqual(true);
   expect(isEmail("-sparta00@naver.com")).toEqual(false);
   expect(isEmail("---------------sparta00@naver.com")).toEqual(false);
+});
+
+test("입력한 이메일 주소의 로컬파트에 영문 대소문자와 숫자, 특수문자는 덧셈기호(+), 하이픈(-), 언더바(_) 3개 외에 다른 값이 존재하면 이메일 형식이 아니다.", () => {
+  expect(isEmail("sparta00@naver.com")).toEqual(true);
+  expect(isEmail("sparta-00@naver.com")).toEqual(true);
+  expect(isEmail("spar_ta-00+@-na--ver-.c-o-m")).toEqual(true);
+  expect(isEmail("sp,a.rt/a00@naver.com")).toEqual(false);
+  expect(isEmail(";sp:a|rt[a00@naver.com")).toEqual(false);
+});
+
+test("입력한 이메일 주소 도메인에 영문 대소문자와 숫자, 점(.), 하이픈(-) 외에 다른 값이 존재하면 이메일 형식이 아니다.", () => {
+  expect(isEmail("sparta00@naver.com")).toEqual(true);
+  expect(isEmail("sparta00@na123ver.com")).toEqual(true);
+  expect(isEmail("sparta-00-@-na--ver-.c-o-m")).toEqual(true);
+  expect(isEmail("-sparta00@na;ve+r.co,m")).toEqual(false);
+  expect(isEmail("sparta00@nave#.^c&o/m")).toEqual(false);
 });
